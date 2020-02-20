e0b67a8d05090241d2a350dd05249b755f8e966c
root <root@ip-172-31-34-199.us-east-2.compute.internal>
2020-02-20 15:52:42 +0000

    remvoed files

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [[ $(git pull | grep 'Already up to date.') = *Already* ]]; then
 	 echo "git is already update"
 else	
 
-git checkout "patch-1"
+#git checkout "patch-1"
 prcoessid=$(ps -ef | grep node | awk '{print $2}')
 kill all $prcoessid
 kill -9 $prcoessid
